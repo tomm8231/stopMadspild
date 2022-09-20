d5c1c6792d7293d28520c3b7d3b11333c667d727
tomm8231 <tomm8231@stud.kea.dk>
2022-09-20 10:50:06 +0200

    Shit load

---
+++ src/main/java/com/example/stopmadspild/StopMadspildApplication.java
@@ -0,0 +1,13 @@
+package com.example.stopmadspild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StopMadspildApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StopMadspildApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/stopmadspild/StopMadspildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stopmadspild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StopMadspildApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
